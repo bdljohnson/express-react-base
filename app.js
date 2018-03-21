32135da33ba94a619aa337871c829a27bbcc3659
Ben Johnson <bdlj0887@gmail.com>
2018-03-21 10:57:05 -0400

    Built for heroku

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require('express');
 const path = require('path');
 
 let app = express();
-app.use(express.static('dist'));
+if(process.env.NODE_ENV !== 'production'){
+    app.use(express.static('dist'));
+} else {
+    app.use(express.static('public'));
+}
+
 
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    if(process.env.NODE_ENV !== 'production'){
+        res.sendFile(path.join(__dirname+'/dist/index.html'));
+    } else {
+        res.sendFile(path.join(__dirname+'/public/index.html'));
+    }
+    
 })
 
 app.listen(3000, ()=>{
